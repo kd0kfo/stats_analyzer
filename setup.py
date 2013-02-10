0bc9c640d861e1437ae390ccbf745d2304f49aed
David Coss <david@davecoss.com>
2013-02-10 16:17:03 -0600

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+ 
+setup (name ='stats_analyzer',
+       version = '1.0',
+       url = 'http://code.davecoss.com',
+       license = 'GPL v3',
+       description = 'Website visitor analyzer',
+       author_email='david@davecoss.com',
+       packages = ['visitors'],
+       )
