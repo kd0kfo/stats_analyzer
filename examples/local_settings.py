9265a2e236e629819b497f412da7afdb82b17538
David Coss <david@davecoss.com>
2013-04-10 20:27:41 -0500

    Added a script that will write a kml file showing the location of visitors.

---
+++ examples/local_settings.py
@@ -0,0 +1,7 @@
+URL = "example.com"
+URL_DIRECTORY ="/RESTful/ip-address/"
+SEARCH_KEYS = ["City","OrgName","OrgId","StateProv","PostalCode","Country","person","country","netname","e-mail","remarks"]
+
+
+HEADER = "/path/to/header.kml"
+FOOTER = "/path/to/footer.kml"
